7cff57dfb06748f8400a66e4198f4465c5fbd964
naji-navas <najidnavas2000@gmail.com>
2024-09-22 09:06:25 +0800

    added readme.md

--- UI/components/HeroSection/HeroSection.tsx
+++ UI/components/HeroSection/HeroSection.tsx
@@ -12,8 +12,7 @@ function HeroSection() {
   return (
     <section className='grid grid-cols-1 md:grid-cols-2 container py-3 md:py-16'>
         <div className='flex flex-col gap-6' data-aos="fade-right">
-            <h2 className='text-white text-5xl leading-normal'>Here is the home screen text come</h2>
-            <p className='text-2xl text-[#acacac]'>some descritpion</p>
+            <h2 className='text-white text-5xl leading-normal'>NFT MARKET PLACE</p>
             <div className='flex space-x-3'>
                 <Link href="/" ><button className='py-4 px-6 bg-[#00a3ff] hover:bg-[#212e48] text-white rounded-md w-40 duration-300'>blahblah </button></Link>
                 <Link href="/"><button className='py-4 px-6 bg-[#212e48] hover:bg-[#00a3ff] text-white rounded-md w-40 duration-300'>blah blah </button></Link>
