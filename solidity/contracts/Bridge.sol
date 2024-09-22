b3e252eb42d848943fd839ee7711399046fcf84d
Vu Vo <vu.voth@gmail.com>
2024-09-22 07:48:10 +0700

    Solidity (#6)
    * process method
    
    * handle
    
    * ensure method

--- solidity/contracts/Bridge.sol
+++ solidity/contracts/Bridge.sol
@@ -5,17 +5,44 @@ import "./dNFT.sol";
 
 // mock bridge
 contract Bridge {
-    DNFT dnft; 
+    DNFT dnft;
 
     constructor() {
-        dnft = new DNFT(address(this));        
+        dnft = new DNFT(address(this));
     }
 
-    function deposit() public {
-        
+    // copy to stackoverflow
+    function bytesToAddress(
+        bytes memory bys
+    ) private pure returns (address addr) {
+        assembly {
+            addr := mload(add(bys, 20))
+        }
+    }
+    
+    function process(
+        bytes calldata metadata,
+        bytes calldata message
+    ) external payable {
+        require(bytesToAddress(metadata) == address(dnft));
+
+        uint256 tokenId = dnft.totalSupply();
+        address receiver = abi.decode(message, (address));
+        dnft.safeMint(receiver, tokenId);
     }
 
-    function withdraw() public {
+    function handle(
+        uint32 _origin,
+        bytes32 _sender,
+        bytes calldata message
+    ) external payable {
+        require(bytesToAddress(metadata) == address(dnft));
 
+        uint256 tokenId = dnft.totalSupply();
+        address receiver = abi.decode(message, (address));
+
+        dnft.safeMint(receiver, tokenId);
     }
+
+    function withdraw() public {}
 }
